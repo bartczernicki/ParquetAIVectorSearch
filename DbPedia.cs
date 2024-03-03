b1103cab999566838365c8705ccb1f2d63d7d50f
Bart Czernicki <bartczernicki@outlook.com>
2024-03-03 18:00:44 -0500

    Process output updates

--- DbPedia.cs
+++ DbPedia.cs
@@ -7,5 +7,6 @@ namespace ParquetAIVectorSearch
         public required string Title { get; set; }
         public required string Text { get; set; }
         public required List<float> Embeddings { get; set; }
+        public required int TokenCount { get; set; } // Uses Title + Text
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using HNSW.Net;
 using ParquetSharp;
+using SharpToken;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 
@@ -12,7 +13,7 @@ namespace ParquetAIVectorSearch
         private const string PARQUET_FILES_DIRECTORY = @"e:\data\dbpedia-entities-openai-1M\";
         private const string PARQUET_FILE_PATH_SUFFIX = @"*.parquet";
         private const int M_PARAMETER = 10; // determines the number of neighbors to consider when adding a new node to the graph
-
+        private const int BATCHSIZE = 10000;
 
 
         static void Main(string[] args)
@@ -27,12 +28,17 @@ namespace ParquetAIVectorSearch
             var GRAPH_FILE_NAME = $"graph_M{M_PARAMETER}.hnsw";
 
             // https://huggingface.co/datasets/KShivendu/dbpedia-entities-openai-1M 
+            // Embeddings are 1536 dimensional - Title + Text
             var parquet_files = Directory.GetFiles(PARQUET_FILES_DIRECTORY, PARQUET_FILE_PATH_SUFFIX);
 
             var parallelOptions = new ParallelOptions
             {
+                // Configure to use 75% of the available processors
                 MaxDegreeOfParallelism = (int) Math.Ceiling((Environment.ProcessorCount * 0.75))
             };
+
+            // Get the encoding for text-embedding-ada-002 or v3, set once as it is an expensive constructor
+            var cl100kBaseEncoding = GptEncoding.GetEncoding("cl100k_base");
 
             // Load Parquet Files in parallel
             Parallel.ForEach(parquet_files, parallelOptions, parquet_file =>
@@ -71,12 +77,16 @@ namespace ParquetAIVectorSearch
 
                             for (int i = 0; i < ids.Length; i++)
                             {
+                                var titleAndText = titles[i] + ' ' + texts[i];
+                                var encodedTokens = cl100kBaseEncoding.Encode(titleAndText);
+
                                 var item = new DbPedia
                                 {
                                     Id = ids[i],
                                     Title = titles[i],
                                     Text = texts[i],
-                                    Embeddings = embeddings[i].Select(x => (float) x).ToList()
+                                    Embeddings = embeddings[i].Select(x => (float) x).ToList(),
+                                    TokenCount = encodedTokens.Count
                                 };
                                 dataSetDbPedias.Add(item);
                             }
@@ -95,14 +105,22 @@ namespace ParquetAIVectorSearch
 
             // Parquet File Load - Get elapsed time & counts
             var endTimeOfParquetLoad = DateTime.Now;
-            Console.WriteLine($"Time Taken: {(endTimeOfParquetLoad - startTime).TotalSeconds} seconds");
-            Console.WriteLine($"Total Records Processed: {recordCount}");
-            Console.WriteLine($"Total Records in Concurrent Bag: {dataSetDbPedias.Count}");
+            var totalTokens = dataSetDbPedias.Sum(x => x.TokenCount);
+            var totalTokensStringWithCommas = totalTokens.ToString("N0");
+            var priceToEncodeAdav2Token = 0.1/1000000; // $0.10 per 1M tokens
+            var priceToEncodeAdav3TokenSmall = 0.02/1000000; // $0.02 per 1M tokens
+            var tokensCostAdav2InDollarsString = (totalTokens * priceToEncodeAdav2Token).ToString("C");
+            var tokensCostAdav3InDollarsString = (totalTokens * priceToEncodeAdav3TokenSmall).ToString("C");
 
+            Console.WriteLine($"Time Taken: {(endTimeOfParquetLoad - startTime).TotalSeconds} seconds");
+            Console.WriteLine($"Total Records Processed: {recordCount.ToString("N0")}");
+            Console.WriteLine($"Total Records in Concurrent Bag: {dataSetDbPedias.Count.ToString("N0")}");
+            Console.WriteLine($"Total Tokens (encoded) in Concurrent Bag: {totalTokensStringWithCommas}");
+            Console.WriteLine($"Total Cost to process tokens (v2) in Concurrent Bag: {tokensCostAdav2InDollarsString}");
+            Console.WriteLine($"Total Cost to process tokens (v3) in Concurrent Bag: {tokensCostAdav3InDollarsString}");
 
             Console.WriteLine($"Build ANN Graph using HNSW...");
             var NumVectors = dataSetDbPedias.Count;
-            var batchSize = 10000;
 
             var hnswGraphParameters = new SmallWorld<float[], float>.Parameters()
             {
@@ -116,40 +134,42 @@ namespace ParquetAIVectorSearch
                 InitialDistanceCacheSize = 100000 * 1024
             };
 
-            var graphs = new List<SmallWorld<float[], float>>();
+            //// Option 1 - Process in parallel all of the batches to a single graph
+            //var graphs = new List<SmallWorld<float[], float>>();
 
-            for (int i = 0; i != 10; i++)
-            {
-                var graphLocal = new SmallWorld<float[], float>(DotProductDistance.DotProductOptimized, DefaultRandomGenerator.Instance,
-                    hnswGraphParameters, threadSafe: true);
+            //for (int i = 0; i != 10; i++)
+            //{
+            //    var graphLocal = new SmallWorld<float[], float>(DotProductDistance.DotProductOptimized, DefaultRandomGenerator.Instance,
+            //        hnswGraphParameters, threadSafe: true);
 
-                graphs.Add(graphLocal);
-            }   
+            //    graphs.Add(graphLocal);
+            //}   
 
-            var graph = new SmallWorld<float[], float>(DotProductDistance.DotProductOptimized, DefaultRandomGenerator.Instance,
-                hnswGraphParameters, threadSafe: true);
+            //var graph = new SmallWorld<float[], float>(DotProductDistance.DotProductOptimized, DefaultRandomGenerator.Instance,
+            //    hnswGraphParameters, threadSafe: true);
 
-            // Enforce order as this is important for the graph to be built correctly
-            var dataSetDbPediasOrdered = dataSetDbPedias.OrderBy(a => a.Id).ToList();
-            var sampleVectors = dataSetDbPediasOrdered.Select(x => x.Embeddings.ToArray()).ToList();
+            //// Enforce order as this is important for the graph to be built correctly
+            //var dataSetDbPediasOrdered = dataSetDbPedias.OrderBy(a => a.Id).ToList();
+            //var sampleVectors = dataSetDbPediasOrdered.Select(x => x.Embeddings.ToArray()).ToList();
 
-            // Chunk the sampleVector into 10 equal batches
-            var dbPediasOrderedPartitions = dataSetDbPediasOrdered.Chunk(sampleVectors.Count / 10).ToList();
+            //// Chunk the sampleVector into 10 equal batches
+            //var dbPediasOrderedPartitions = dataSetDbPediasOrdered.Chunk(sampleVectors.Count / 10).ToList();
 
-            var numberOfBatches = (int) Math.Ceiling((double) NumVectors / batchSize);
+            //var numberOfBatches = (int) Math.Ceiling((double) NumVectors / BATCHSIZE);
 
-            Parallel.For(0, dbPediasOrderedPartitions.Count, parallelOptions, i =>
-            {
-                var partition = dbPediasOrderedPartitions[i].Select(x => x.Embeddings.ToArray()).ToList();
-                var numberOfBatches = (int) Math.Ceiling((double)partition.Count / batchSize);
+            //Parallel.For(0, dbPediasOrderedPartitions.Count, parallelOptions, i =>
+            //{
+            //    var partition = dbPediasOrderedPartitions[i].Select(x => x.Embeddings.ToArray()).ToList();
+            //    var numberOfBatches = (int) Math.Ceiling((double)partition.Count / BATCHSIZE);
 
-                for (int j = 0; j < numberOfBatches; j++)
-                {
-                    var partitionBatch = partition.Skip(j * batchSize).Take(batchSize).ToList();
-                    graphs[i].AddItems(partitionBatch);
-                    Console.WriteLine($"Vector Partition: {i+1}, Added {j + 1} of {numberOfBatches} \n");
-                }
-            });
+            //    for (int j = 0; j < numberOfBatches; j++)
+            //    {
+            //        var partitionBatch = partition.Skip(j * BATCHSIZE).Take(BATCHSIZE).ToList();
+            //        graphs[i].AddItems(partitionBatch);
+            //        Console.WriteLine($"Vector Partition: {i+1}, Added {j + 1} of {numberOfBatches} \n");
+            //    }
+            //});
+
 
             //// Option 2 - Process Sequentially all of the batches to a single graph
             //for (int i = 0; i < numberOfBatches; i++)
@@ -157,20 +177,24 @@ namespace ParquetAIVectorSearch
             //    var stopWatchBatch = Stopwatch.StartNew();
             //    stopWatchBatch.Start();
 
-            //    var batch = sampleVectors.Skip(i * batchSize).Take(batchSize).ToList();
+            //    var batch = sampleVectors.Skip(i * BATCHSIZE).Take(BATCHSIZE).ToList();
             //    graph.AddItems(batch);
             //    Console.WriteLine($"\nAdded {i + 1} of {numberOfBatches} \n");
 
             //    Console.WriteLine($"Time Taken for Batch {i+1}: {stopWatchBatch.ElapsedMilliseconds} ms.");
             //}
 
-            var endTimeOfGraphBuild = DateTime.Now;
-            Console.WriteLine($"Time Taken to build ANN Graph: {(endTimeOfGraphBuild - endTimeOfParquetLoad).TotalSeconds} seconds");
+            //var endTimeOfGraphBuild = DateTime.Now;
+            //Console.WriteLine($"Time Taken to build ANN Graph: {(endTimeOfGraphBuild - endTimeOfParquetLoad).TotalSeconds} seconds");
 
-            for(int i = 0; i != 10; i++)
-            {
-                SaveHNSWGraph(graphs[i], PARQUET_FILES_DIRECTORY, $"graph_M{M_PARAMETER}_partition{i+1}.hnsw");
-            }
+            //for(int i = 0; i != 10; i++)
+            //{
+            //    SaveHNSWGraph(graphs[i], PARQUET_FILES_DIRECTORY, $"graph_M{M_PARAMETER}_partition{i+1}.hnsw");
+            //}
+
+
+
+
 
             //var searchVector = sampleVectors[0];// RandomVectors(1536, 1)[0];
             //var results = graph.KNNSearch(searchVector, 20);
